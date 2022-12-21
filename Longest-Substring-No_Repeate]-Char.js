05c33a931fb60ba0458617f519fc75aa2cdb8d67
chetankorde <ckorde@crosscountry.com>
2022-12-21 10:44:00 +0530

    programs complexity added

--- Longest-Substring-No_Repeate]-Char.js
+++ Longest-Substring-No_Repeate]-Char.js
@@ -16,4 +16,5 @@
         }
         console.log(`Generating substring with no repeating char and max length of substring from given parent string - \n --> given string \t ${string} \n --> Max length of substring is - \t ${resultSubStrCount} \n --> strings can form are - \t ${resultSubArr.filter(item => (item.length==resultSubStrCount) ? item : '')}`);
     }
-)('chetan korde patil name taken for testing')
+)('chetan korde patil name taken for testing');
+//Complexity of this code is O(n^2) ---> 2 for loops are used

a701bf247098272cbaf44a009c2bb9e655c81ed3
chetankorde <ckorde@crosscountry.com>
2022-12-23 11:15:01 +0530

    Try new logic for new solution on battle file
    New DSA problem & logic added - Median of 2 *sorted* array and merge it to make new complete array

---
+++ Algorithm_Information.js
@@ -0,0 +1,26 @@
+/*
+
+https://towardsdatascience.com/logarithms-exponents-in-complexity-analysis-b8071979e847#:~:text=Logarithmic%20time%20complexity%20log(n,Example%3A%20binary%20search.
+
+https://medium.com/@StueyGK/algorithm-time-complexity-and-big-o-notation-51502e612b4d
+
+https://devopedia.org/algorithmic-complexity#:~:text=In%20complexity%20analysis%2C%20only%20the,are%20concerned%20only%20about%20asymptotic.
+
+https://www.javatpoint.com/daa-complexity-of-algorithm
+
+**Difference between Searching and Sorting Algorithm:
+
+1.	Searching Algorithms are designed to retrieve an element from any data structure where it is used.	
+    A Sorting Algorithm is used to arranging the data of list or array into some specific order.
+2.	These algorithms are generally classified into two categories i.e. Sequential Search and Interval Search.	
+    There are two different categories in sorting. These are Internal and External Sorting.
+3.	The worst-case time complexity of searching algorithm is O(N).	
+    The worst-case time complexity of many sorting algorithms like Bubble Sort, Insertion Sort, Selection Sort, and Quick Sort is O(N2).
+4.	There is no stable and unstable searching algorithms.	
+    Bubble Sort, Insertion Sort, Merge Sort etc are the stable sorting algorithms whereas Quick Sort, Heap Sort etc are the unstable sorting algorithms.
+5.	The Linear Search and the Binary Search are the examples of Searching Algorithms.	
+    The Bubble Sort, Insertion Sort, Selection Sort, Merge Sort, Quick Sort etc are the examples of Sorting Algorithms.
+
+    */
+   
+//https://medium.com/learning-python-programming-language/sorting-algorithms-insertion-sort-selection-sort-quick-sort-merge-sort-bubble-sort-4f23bda6f37a
